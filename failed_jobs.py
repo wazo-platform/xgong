5112376c0b5f5c1653c41d0181879c714fbbd7f2
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-04-08 10:31:16 -0400

    read config from file

--- failed_jobs.py
+++ failed_jobs.py
@@ -5,34 +5,42 @@ import sys
 import datetime
 import re
 
-URL = "http://jenkins.server.com/api/json"
-PERSIST = "/tmp/last_failed_jobs.json"
+from configparser import ConfigParser
+
+CONFIG_PATH = '/etc/jenkins-gong/config.ini'
 FAIL_REGEX = re.compile(r"^(red|yellow)")
+
+
+def load_config(filepath):
+    parser = ConfigParser()
+    parser.readfp(open(filepath))
+    return parser
 
 
 def fetch_jobs(url):
     response = requests.get(url)
-    return json.loads(response.text)
+    data = json.loads(response.text)
+    return data['jobs']
 
 
 def filter_failed_jobs(jobs):
     return [job['name'] for job in jobs if FAIL_REGEX.match(job['color'])]
 
 
-def new_failed_jobs(failed_jobs):
-    old_jobs = fetch_old_jobs()
+def new_failed_jobs(failed_jobs, history):
+    old_jobs = fetch_old_jobs(history)
 
-    with open(PERSIST, mode='w') as f:
+    with open(history, mode='w') as f:
         f.write(json.dumps(failed_jobs))
 
     return set(failed_jobs) - set(old_jobs)
 
 
-def fetch_old_jobs():
-    if not os.path.exists(PERSIST):
+def fetch_old_jobs(history):
+    if not os.path.exists(history):
         return []
 
-    with open(PERSIST) as f:
+    with open(history) as f:
         return json.loads(f.read())
 
 
@@ -43,10 +51,14 @@ def print_failed_jobs(jobs):
 
 
 if __name__ == '__main__':
-    response = fetch_jobs(URL)
-    jobs = response['jobs']
+    config = load_config(CONFIG_PATH)
+
+    jenkins_url = config.get('jenkins-gong', 'url')
+    history = config.get('jenkins-gong', 'history')
+
+    jobs = fetch_jobs(jenkins_url)
     failed = filter_failed_jobs(jobs)
-    new_failed = new_failed_jobs(failed)
+    new_failed = new_failed_jobs(failed, history)
 
     if new_failed:
         print_failed_jobs(new_failed)
